0a2064da5392744683ef0db6af9874d0aa350c9b
CareF <CareF.Lm@gmail.com>
2019-11-27 20:17:06 -0500

    to long double

--- sumsqrt.c
+++ sumsqrt.c
@@ -6,27 +6,28 @@
 #define HALFIDX (((idxbits)1<<(NMAX/2)))
 #define REMAIN (((idxbits)1<<((NMAX+1)/2)))
 typedef long long idxbits;
+typedef long double sfloat;
 
-double idxToSum(idxbits idx, int nmin, int nmax) {
+sfloat idxToSum(idxbits idx, int nmin, int nmax) {
     int i;
-    double sum = 0;
+    sfloat sum = 0;
     for(i=nmin; i<=nmax; i++) {
         if(idx%2)
-            sum += sqrt(i);
+            sum += sqrtl(i);
         idx >>= 1;
     }
     return sum;
 }
 
-/*RAND_MAX = 2^31 - 1 here*/
+/*RAND_MAX = 2^31 - 1 here for gcc 9.2 on x86_64*/
 #define idxRand (((idxbits)rand())*((idxbits)RAND_MAX+1) + (idxbits)rand())
-void idxQSort(const double *nums, idxbits *idxs, idxbits l, idxbits r) {
+void idxQSort(const sfloat *nums, idxbits *idxs, idxbits l, idxbits r) {
     /*Quick sort indexes for nums*/
     if(l < r-1) {
         idxbits i = l;
         idxbits j = r-1;
         idxbits m = l + idxRand%(r-l);
-        double x = nums[idxs[m]];
+        sfloat x = nums[idxs[m]];
         m = i;
         while(m <= j) {
             idxbits tmp;
@@ -51,7 +52,7 @@ void idxQSort(const double *nums, idxbits *idxs, idxbits l, idxbits r) {
     }
 }
 
-idxbits idxBiSect(const double *nums, const idxbits *idxs, double target) {
+idxbits idxBiSect(const sfloat *nums, const idxbits *idxs, sfloat target) {
     /*return min idxs s.t. nums[idx] >= target*/
     idxbits lo=0;
     idxbits hi=HALFIDX;
@@ -68,13 +69,14 @@ idxbits idxBiSect(const double *nums, const idxbits *idxs, double target) {
 }
 
 int main() {
-    double *halfSum = malloc(HALFIDX*sizeof(double));
+    /*Have to malloc or segmentation fault*/
+    sfloat *halfSum = malloc(HALFIDX*sizeof(sfloat));
     idxbits *halfIdx = malloc(HALFIDX*sizeof(idxbits));
     idxbits n;
-    double target = idxToSum(((idxbits)1<<NMAX)-1, 1, NMAX)/2;
-    double res = target;
+    sfloat target = idxToSum(((idxbits)1<<NMAX)-1, 1, NMAX)/2;
+    sfloat res = target;
     idxbits residx;
-    printf("Half of sum sqrt %.16f\n", target);
+    printf("Half of sum sqrt %.16llf\n", target);
 
     for(n=0; n<HALFIDX; n++){
         halfIdx[n] = n;
@@ -84,14 +86,14 @@ int main() {
     #pragma omp single
     idxQSort(halfSum, halfIdx, 0, HALFIDX);
     /* for(n=0; n<HALFIDX; n++) */
-    /*     printf("%f\n", n, halfSum[halfIdx[n]]); */
+    /*     printf("%llf\n", n, halfSum[halfIdx[n]]); */
 
     #pragma omp parallel
     {
-        double res_local = res;
+        sfloat res_local = res;
         idxbits residx_local;
         for(n=0; n<REMAIN; n++){
-            double sum = idxToSum(n, NMAX/2+1, NMAX);
+            sfloat sum = idxToSum(n, NMAX/2+1, NMAX);
             idxbits l = idxBiSect(halfSum, halfIdx, target-sum);
             if(l < HALFIDX && sum + halfSum[l] - target < res_local) {
                 res_local = sum + halfSum[l] - target;
@@ -112,13 +114,25 @@ int main() {
         if(residx & ((idxbits)1<<(n-1)))
             printf("%d ", n);
     }
-    printf(", \ndiff=%.16e\n", res);
+    printf(", \ndiff=%.16lle\n", res);
+    free(halfSum);
+    free(halfIdx);
     return 0;
 }
 
 /*
- * Output:
+ * Output with double:
  * Half of sum sqrt 119.5179003017603776
  * min sum of sqrt (566001980646974): 2 3 4 5 6 10 11 14 16 17 18 19 20 21 22 23 24 26 28 29 32 34 35 39 40 42 50 ,
  * diff=9.2370555648813024e-14
+ *
+ * with long double:
+ * Half of sum sqrt 119.5179003017603917
+ * min sum of sqrt (566001980646974): 2 3 4 5 6 10 11 14 16 17 18 19 20 21 22 23 24 26 28 29 32 34 35 39 40 42 50 ,
+ * diff=7.2719608112947753e-14
+ *
+ * with long double and sqrtl:
+ * Half of sum sqrt 119.5179003017603922
+ * min sum of sqrt (566001980646974): 2 3 4 5 6 10 11 14 16 17 18 19 20 21 22 23 24 26 28 29 32 34 35 39 40 42 50 ,
+ * diff=7.1491423891956174e-14
  */
